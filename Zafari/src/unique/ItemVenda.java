c27cb11663732469b198a29d5c18ffecdf08d2df
Israel Deorce <israel.idvj@gmail.com>
2017-03-09 02:40:37 -0300

    Classe ItemVenda parcialmente implementada

---
+++ Zafari/src/unique/ItemVenda.java
@@ -0,0 +1,42 @@
+package unique;
+
+public class ItemVenda {
+	
+	private Produto produto;
+	private int quantidade;
+	private double valorTotal;
+	
+	public ItemVenda(Produto produto){		
+		this.produto = produto;
+		this.quantidade = 1;
+		calculaValorTotal();
+	}
+	public ItemVenda(Produto produto, int quantidade){
+		this.produto = produto;		
+		this.quantidade = quantidade;
+		calculaValorTotal();
+	}
+	public Produto getProduto() {
+		return produto;
+	}	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+		calculaValorTotal();
+	}
+	public double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	public void calculaValorTotal(){
+		double valorUnitario = produto.getPreco();		
+		valorTotal = valorUnitario * quantidade;
+	}
+	
+
+}
