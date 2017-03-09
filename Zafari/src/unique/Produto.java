67ef492ac7e282c9c762a3779d8a30f10a284c22
Israel Deorce <israel.idvj@gmail.com>
2017-03-09 02:26:46 -0300

    Classe Produto parcialmente implementada

---
+++ Zafari/src/unique/Produto.java
@@ -0,0 +1,32 @@
+package unique;
+
+public class Produto {
+	
+	private double preco;
+	private String nome;
+	private String serial;
+	
+	public Produto(){
+		
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSerial() {
+		return serial;
+	}
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+
+}
