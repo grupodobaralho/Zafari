f6176f310e86f26920d566ab8608cb77959f0c48
Alexandre Agustini <alexandre.agustini@pucrs.br>
2017-03-17 16:38:04 -0300

    Algumas alterações para discussão em aula

--- Zafari/src/unique/App.java
+++ Zafari/src/unique/App.java
@@ -4,19 +4,19 @@ public class App {
 	
 	public static void main(String[] args){
 		
-		//Preparando simulaÁ„o:
+		//Preparando simula√ß√£o:
 		
 		Produto produto = new Produto(2.5, "Chocolate", "12340");
 		Produto produto1 = new Produto(4.95, "Coca-cola 2L", "12341");
-		Produto produto2 = new Produto(7.5, "¡gua de Coco", "12342");
+		Produto produto2 = new Produto(7.5, "√Ågua de Coco", "12342");
 		Produto produto3 = new Produto(3.5, "Cachorro-quente", "12343");
 		Produto produto4 = new Produto(12.5, "Frango", "12344");
-		Produto produto5 = new Produto(1.25, "¡gua 1L", "12345");
+		Produto produto5 = new Produto(1.25, "√Ågua 1L", "12345");
 		Produto produto6 = new Produto(3.12, "Cookies", "12346");
 		Produto produto7 = new Produto(4.5, "Batata Frita", "12347");
 		Produto produto8 = new Produto(5.95, "Maionese", "12348");
 		Produto produto9 = new Produto(5.95, "Catch-UP", "12349");
-		Produto produto10 = new Produto(2.5, "PicolÈ de Manga", "66612");
+		Produto produto10 = new Produto(2.5, "Picol√© de Manga", "66612");
 		
 		Nota nota = new Nota();			
 		
